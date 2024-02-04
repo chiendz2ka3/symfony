d893ac449f02e404e08b091dfac0324534c7dba6
ChienTQ2003 <anhchienbs.1@gmail.com>
2024-02-04 11:02:02 +0700

    commit

--- web/js/multiclick-stopper.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    $("form").submit(function() {
-        $('button[type=submit], input[type=submit]').prop('disabled',true);
-    });
-});
--- web/js/shopping-cart.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    setTimeout(function(){$('.alert-success').fadeOut()},2000);
-    setTimeout(function(){$('.alert-danger').fadeOut()},4000);
-
-    $("form").submit(function() {
-        //$('button[type=submit], input[type=submit]').prop('disabled',true);
-    });
-
-});
